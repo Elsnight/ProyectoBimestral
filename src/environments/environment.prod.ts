9f77e9682e87cf5fe2e787e0a016d97ecc67a13a
Miguel Cuenca <ecuenca4@gmail.com>
2022-07-08 01:52:10 -0500

    implementacion de ng add @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'soccer-dfd62',
+    appId: '1:883877846641:web:ef13bb86cd542d36aec5d0',
+    storageBucket: 'soccer-dfd62.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBNpDsIW_OcXkHakyDk6JL4NxhXjeNbEbY',
+    authDomain: 'soccer-dfd62.firebaseapp.com',
+    messagingSenderId: '883877846641',
+  },
   production: true
 };
